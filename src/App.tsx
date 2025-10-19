063c555921a6cba65baff4e326187f25dd5e85b5
Ryan Schachte <code@ryan-schachte.com>
2025-10-19 16:45:00 +0100

    init

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="w-full h-full">
+      {/* Your app content goes here */}
+    </div>
+  )
+}
+
+export default App
---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import markdown from 'vite-plugin-markdown'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    markdown({ mode: ['html', 'react'] })
+  ],
+})
